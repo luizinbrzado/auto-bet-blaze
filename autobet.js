fea37484ea5df89fbbb4ee0444086ed5694f96dc
luizinbrzado <luiztrineves@gmail.com>
2022-05-28 16:14:56 -0300

    j

--- autobet.js
+++ autobet.js
@@ -100,7 +100,7 @@ console.log("Rodando web scraping");
         }
     );
 
-    await driver.findElement(webdriver.By.xpath('//*[@id="header"]/div[2]/div/div[2]/div/div/div[1]/a')).click()
+    await driver.findElement(webdriver.By.xpath('//*[@id="header"]/div[2]/div/div[2]/div/div/div[1]')).click()
     await driver.sleep(5000)
     await driver.findElement(webdriver.By.xpath('//*[@id="auth-modal"]/div[2]/form/div[1]/div/input')).sendKeys(process.env.USER)
     await driver.findElement(webdriver.By.xpath('//*[@id="auth-modal"]/div[2]/form/div[2]/div/input')).sendKeys(process.env.PASS)
